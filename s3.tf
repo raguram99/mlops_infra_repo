ce0c841df0b330ac91b29c0c3028c422207dd75e
raguram99 <ramcloud88@gmail.com>
2023-04-14 03:51:29 +0530

    2nd commit

--- s3.tf
+++ s3.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_version = "~> 1.3" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-
-  region  = "ap-south-1"
-  profile = "default"
-
-}
-
 resource "aws_s3_bucket" "this_bucket" {
   bucket = "demo-bucket-ddd-pro-ragu007"
 }
